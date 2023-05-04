d7a5103bb92835c0f13d44131055519a1e323576
ntua-el19707 <el19707@mail.ntua.gr>
2023-05-04 12:48:23 +0300

    add the new  commpents  in user module

--- microservice02/Frontend/src/app/user/user.module.ts
+++ microservice02/Frontend/src/app/user/user.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MyinfoComponent } from './myinfo/myinfo.component';
-import { MatCardModule } from '@angular/material/card';
-import { WelcomeComponent } from './welcome/welcome.component';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MyinfoComponent } from "./myinfo/myinfo.component";
+import { MatCardModule } from "@angular/material/card";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { BuyCreditsComponent } from "./buy-credits/buy-credits.component";
+import { CreditsCardComponent } from "./credits-card/credits-card.component";
+import { MatGridListModule } from "@angular/material/grid-list";
 //TODO: later create a componet for buying credits.
 
 @NgModule({
   declarations: [
     MyinfoComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    BuyCreditsComponent,
+    CreditsCardComponent,
   ],
-  imports: [
-    CommonModule, MatCardModule
-  ]
+  imports: [CommonModule, MatCardModule, MatCardModule, MatGridListModule],
 })
-export class UserModule { }
+export class UserModule {}
