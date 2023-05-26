5fd6f8ee121bccfa5be79e344b22b9f1af72186d
ntua-el18716 <116654394+ntua-el18716@users.noreply.github.com>
2023-05-26 18:12:26 +0300

    Add files via upload

--- microservice06/DependencyWheel/utils/lib/buildFunctions/dataBuild.js
+++ microservice06/DependencyWheel/utils/lib/buildFunctions/dataBuild.js
@@ -1,45 +1,45 @@
 
 function buildDependancyWheelOptions(data) {
-  let options = {
-  };
-
-  if (data.title) {
-    options.title = {};
-    if (data.title.text) {
-        options.title.text = data.title.text;
-    }
-  };
-
-  options.accessibility = {
-    point: {
-      valueDescriptionFormat: '{index}. From {point.from} to {point.to}: {point.weight}.'
-    }
-  };
-
-  options.series =[ {
-    keys: data.series.keys,
-    data: data.series.data,
-    type: 'dependencywheel',
-    name: 'Dependency wheel series',
-    dataLabels: {
-      color: '#333',
-      style: {
-        textOutline: 'none'
-      },
-      textPath: {
-        enabled: true
-      },
-      distance: 10
-    },
-    size: '95%'
-  }]
-
+    let options = {
+    };
   
-
-
-
-  //TODO  impment  options  builder
-  return options;
-}
-
-module.exports = { buildDependancyWheelOptions };
+    if (data.title) {
+      options.title = {};
+      if (data.title.text) {
+          options.title.text = data.title.text;
+      }
+    };
+  
+    options.accessibility = {
+      point: {
+        valueDescriptionFormat: '{index}. From {point.from} to {point.to}: {point.weight}.'
+      }
+    };
+  
+    options.series =[ {
+      keys: data.series.keys,
+      data: data.series.data,
+      type: 'dependencywheel',
+      name: data.series.name,
+      dataLabels: {
+        color: '#333',
+        style: {
+          textOutline: 'none'
+        },
+        textPath: {
+          enabled: true
+        },
+        distance: 10
+      },
+      size: '95%'
+    }]
+  
+    
+  
+  
+  
+    //TODO  impment  options  builder
+    return options;
+  }
+  
+  module.exports = { buildDependancyWheelOptions };
